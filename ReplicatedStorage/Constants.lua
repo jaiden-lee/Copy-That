da75afe323bacfeef6391cc7f9446805b3da0ce5
jaiden-lee <96359051+jaiden-lee@users.noreply.github.com>
2023-01-02 15:04:33 -0500

    Create Constants.lua

---
+++ ReplicatedStorage/Constants.lua
@@ -0,0 +1,54 @@
+local constants = {}
+
+constants.PURCHASE_COINS_ID = {
+	["Small_100"] = 1265919568,
+	["Med_250"] = 1265919612,
+	["Lar_750"] = 1265919706,
+	["Hug_1500"] = 1265919843,
+	["Mon_3750"] = 1265919890,
+	["Ult_10000"] = 1265919956
+}
+
+constants.SHORTER_EMOTE_COOLDOWN_ID = 49433238
+
+constants.PURCHASE_COINS_AMOUNT = {
+	[1265919568] = 50,
+	[1265919612] = 125,
+	[1265919706] = 375,
+	[1265919843] = 750,
+	[1265919890] = 1875,
+	[1265919956] = 5000
+}
+
+constants.EMOTE_COSTS = {
+	["Smile"] = 0,
+	["ThumbsUp"] = 25,
+	["ThumbsDown"] = 25,
+	["Angry"] = 50,
+	["BruhFace"] = 50,
+	["CurseFace"] = 250,
+	["DisappointedFace"] = 50,
+	["EvilFace"] = 50,
+	["LargeSmile"] = 50,
+	["Star"] = 50,
+	["Sunglasses"] = 50,
+	["WhyFace"] = 50,
+	["Yawn"] = 50,
+	["Laugh"] = 750,
+	["AmogUs"] = 750,
+	["Clown"] = 1500,
+	["BabyFace"] = 500,
+	["Stoneface"] = 400,
+	["SoundBruhFace"] = 750,
+	["Golira"] = 2000,
+	["Orangutan"] = 1000,
+	["Spongebob"] = 500,
+	["MrKrabs"] = 750,
+	["Oof"] = 150,
+	["TalkingBen"] = 250,
+	["TechnobladeNeverDies"] = "CODE",
+	["Secret"] = "CODE",
+	["Release"] = "CODE"
+}
+
+return constants
