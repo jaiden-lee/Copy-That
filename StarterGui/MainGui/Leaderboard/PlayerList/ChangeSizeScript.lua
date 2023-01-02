98b9d6c1f5e6503dba9a57677f70c7031a706960
jaiden-lee <96359051+jaiden-lee@users.noreply.github.com>
2023-01-02 15:16:17 -0500

    Create ChangeSizeScript.lua

---
+++ StarterGui/MainGui/Leaderboard/PlayerList/ChangeSizeScript.lua
@@ -0,0 +1,24 @@
+local listLayout = script.Parent.UIListLayout
+
+
+local function changeCanvas ()
+	local abSize = script.Parent.AbsoluteSize.Y
+	script.Parent.CanvasSize = UDim2.new(0, 0, 0, listLayout.AbsoluteContentSize.Y)
+	listLayout.Padding = UDim.new(0, math.ceil(8/272*abSize))
+	for i,v in pairs(script.Parent:GetChildren()) do
+		if v:IsA("Frame") then
+			v.Size = UDim2.new(1,0,0,math.ceil(35/272*abSize))
+		end
+	end
+end
+
+script.Parent:GetPropertyChangedSignal("AbsoluteSize"):Connect(function()
+	changeCanvas()
+end)
+listLayout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
+	changeCanvas()
+end)
+
+changeCanvas()
+
+script.Parent.ChildAdded:Connect(changeCanvas)
